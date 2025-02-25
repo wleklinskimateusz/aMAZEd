b1c9ae2acc33f1eaac20fe213f20927996040508
TomekMaciocha <tmaciocha95@gmail.com>
2025-02-25 16:34:16 +0100

    changes to pass the unit tests

--- src/generator/wilson.py
+++ src/generator/wilson.py
@@ -6,7 +6,7 @@ from random import randint
 class Wilson(Maze):
     """A class for generating a maze using the Wilson algorithm"""
 
-    def __init__(self, maze_size=(10, 10)) -> None:
+    def __init__(self, maze_size: tuple[int, int] = (10, 10)) -> None:
         if maze_size[0] < 2 or maze_size[1] < 2:
             raise ValueError("Invalid maze size, must be at least 2x2")
         self.graph: defaultdict[tuple[int, int], set[tuple[int, int]]] = defaultdict(
@@ -24,10 +24,10 @@ class Wilson(Maze):
         self.unconnected.remove(
             (randint(0, self.maze_size[0] - 1), randint(0, self.maze_size[1] - 1))
         )
-        while len(self.unconnected) > 0:
-            self.Add_Branch()
+        while not self.Add_Branch():
+            pass
 
-    def Add_Branch(self):
+    def Add_Branch(self) -> int:
         """Adds a new branch to the maze if possible"""
         if len(self.unconnected) == 0:
             return -1
@@ -44,7 +44,8 @@ class Wilson(Maze):
             if (
                 proposed_expansion[0] not in range(self.maze_size[0])
                 or proposed_expansion[1] not in range(self.maze_size[1])
-                or proposed_expansion == branch[-2]
+                or len(branch) > 1
+                and proposed_expansion == branch[-2]
             ):
                 continue
             elif proposed_expansion in branch:
@@ -57,3 +58,5 @@ class Wilson(Maze):
         for iter in range(len(branch) - 1):
             self.add_connection(branch[iter], branch[iter + 1])
             self.unconnected.remove(branch[iter])
+
+        return 0
