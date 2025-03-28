4a31e67328ea96cceef430e1556ede951a698b78
wleklinskimateusz <wleklinskimateusz05@gmail.com>
2025-03-28 21:48:48 +0100

    fix: type error

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from generator.generator import Generator
 def main() -> None:
     generator_algorithms: list[type[Generator]] = []
     for GeneratorClass in generator_algorithms:
-        generator = GeneratorClass(10, 10, (0, 0), (9, 9))
+        generator = GeneratorClass(10, 10)
         maze = generator.generate()
         print(maze)
 
