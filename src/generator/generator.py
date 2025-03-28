206ddd48ba59a7977ed4ee0ed6b6cd9d6fda1f15
wleklinskimateusz <wleklinskimateusz05@gmail.com>
2025-03-28 21:44:20 +0100

    fix: remove start and end from generator class

--- src/generator/generator.py
+++ src/generator/generator.py
@@ -8,21 +8,15 @@ class Generator(ABC):
         self,
         height: int,
         width: int,
-        start: tuple[int, int],
-        end: tuple[int, int],
     ) -> None:
         """Initialize the generator with maze parameters.
 
         Args:
             height: Height of the maze
             width: Width of the maze
-            start: Starting point coordinates (x, y)
-            end: End point coordinates (x, y)
         """
         self.height = height
         self.width = width
-        self.start = start
-        self.end = end
 
     @abstractmethod
     def generate(self) -> Maze:
